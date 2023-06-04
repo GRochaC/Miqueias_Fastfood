c7187f039bf081d815a5a553eb7d18a591c6c10d
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-04 13:22:22 -0300

    Add files via upload

---
+++ MiqueiasFastFood/src/MiqueiasFastFood/ItemPedido.java
@@ -0,0 +1,34 @@
+package MiqueiasFastFood;
+
+/**
+ *
+ * @author guilherme
+ */
+public class ItemPedido {
+    private int quantidade;
+    private double subtotal;
+    private final Item item;
+    
+    public ItemPedido(int quantidade, Item item) {
+        this.quantidade = quantidade;
+        this.item = item;
+        this.subtotal = item.getPreco()*quantidade;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+        this.subtotal = quantidade*item.getPreco();
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+}
