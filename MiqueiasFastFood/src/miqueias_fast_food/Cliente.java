742a23c503055b7d3fc2db177cb4de81b846d26c
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-04 18:48:15 -0300

    Update Cliente.java

--- MiqueiasFastFood/src/miqueias_fast_food/Cliente.java
+++ MiqueiasFastFood/src/miqueias_fast_food/Cliente.java
@@ -1,13 +1,38 @@
-package MiqueiasFastFood;
+package miqueias_fast_food;
 
 /**
  *
  * @author archago
  */
-public class Cliente extends Pessoa {
+public class Cliente extends Pessoa{
+    private boolean takeOut;
     private String dataDeAniversario;
     private String telefone;
+    private Mesa mesa;
+    private final Pedido pedido;
 
+    public Cliente(boolean takeOut, String dataDeAniversario, String telefone, String cpf, String nome) {
+        super(cpf, nome);
+        this.takeOut = takeOut;
+        this.dataDeAniversario = dataDeAniversario;
+        this.telefone = telefone;
+        this.mesa = null;
+        this.pedido = new Pedido(this, 0, this.takeOut);
+    }
+
+    public Cliente(boolean takeOut, String dataDeAniversario, String telefone, Mesa mesa, String cpf, String nome) {
+        super(cpf, nome);
+        this.takeOut = takeOut;
+        this.dataDeAniversario = dataDeAniversario;
+        this.telefone = telefone;
+        this.mesa = mesa;
+        this.pedido = new Pedido(this, 0, this.takeOut);
+    }
+    
+    public Pedido getPedido() {
+        return this.pedido;
+    }
+    
     public String getDataDeAniversario() {
         return dataDeAniversario;
     }
@@ -22,6 +47,22 @@ public class Cliente extends Pessoa {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    public boolean isTakeOut() {
+        return takeOut;
+    }
+
+    public void setTakeOut(boolean takeOut) {
+        this.takeOut = takeOut;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
     }
     
 }
