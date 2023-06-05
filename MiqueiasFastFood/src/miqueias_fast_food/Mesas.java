2fecb5daa2399c54a180c99f76d1ac468ad2c129
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-05 19:05:25 -0300

    Correção e update da classe Mesas.java

--- MiqueiasFastFood/src/miqueias_fast_food/Mesas.java
+++ MiqueiasFastFood/src/miqueias_fast_food/Mesas.java
@@ -5,21 +5,42 @@ import java.util.*;
 public class Mesas{
     private static ArrayList<ArrayList<Mesa>> mesas;   
     
-    // verifica a disponibilidade de todas as mesas
-    public void checarDisponibilidade(){
-        for(ArrayList<Mesa> fileiraDeMesas : mesas)
-            for(Mesa mesa : fileiraDeMesas)
-                System.out.printf(mesa.getDisponivel() + " ");
-            System.out.println();
+    // verifica a disponibilidade da mesa utilizando indices da matriz
+    public static boolean checarDisponibilidade(int i, int j){
+        return mesas.get(i).get(j).getDisponivel();
+    }
+    
+    // verifica a disponibilidade da mesa utilizando o identificador da mesa
+    public static boolean checarDisponibilidade(int identificador) {
+        return mesas.get(identificador/4).get((identificador/4) * 4 - identificador).getDisponivel();
     }
 
-    // Função para marcar uma mesa como ocupada
-    public void ocuparMesa(int i, int j){
+    // Função para marcar uma mesa como ocupada utilizando indices da matriz
+    public static void ocuparMesa(int i, int j){
         mesas.get(i).get(j).toggleDisponivel();
+    }
+    
+    // Função para marcar uma mesa como ocupada utilizando o identificador da mesa
+    public static void ocuparMesa(int identificador) {
+        mesas.get(identificador/4).get((identificador/4) * 4 - identificador).toggleDisponivel();
     }
     
     // Inicializa o conjunto de mesas, inicialmente com 16 mesas no total, cada 
     // uma com tamanho 2
+    public static void gerarMesas() {
+        int identificador = 0;
+        for(int i = 0; i < 4; i++) {
+            ArrayList<Mesa> fileira = new ArrayList<>();
+            for(int j = 0; j < 4; j++) {
+                Mesa mesa = new Mesa(identificador, (byte) 2, true);
+                fileira.add(mesa);
+                ++identificador;
+            }
+            mesas.add(fileira);
+        }
+    }
+    
+    /*
     public static void main(String[] args){
         mesas = new ArrayList<>();
         int identificador = 0;
@@ -32,4 +53,5 @@ public class Mesas{
               }
         }         
     }
+    */
 }
