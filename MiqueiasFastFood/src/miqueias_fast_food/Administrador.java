ba02f92dc1e9845ee027c7561037b308b7b1f4a4
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-14 14:19:16 -0300

    documentação da classe FUNCIONARIO
    lembrar de trocar o nome da classe para Funcionario

--- MiqueiasFastFood/src/miqueias_fast_food/Administrador.java
+++ MiqueiasFastFood/src/miqueias_fast_food/Administrador.java
@@ -1,31 +1,42 @@
 package miqueias_fast_food;
 
 /**
- *
  * @author archago
+ * Classe que representa o funcionário no sistema
  */
+
+// TROCAR O NOME DA CLASSE PARA FUNCIONARIO
+
 public class Administrador extends Pessoa {
+    // login do funcionário
     private String login;
+    
+    // senha do funcionário
     private String senha;
 
+    // Construtor da classe Funcionario
     public Administrador(String login, String senha, String cpf, String nome) {
         super(cpf, nome);
         this.login = login;
         this.senha = senha;
     }
     
+    // retorna o login do funcionário
     public String getLogin() {
         return login;
     }
 
+    // define um novo login para o funcionário
     public void setLogin(String login) {
         this.login = login;
     }
 
+    // retorna a senha do funcionário
     public String getSenha() {
         return senha;
     }
 
+    // define uma nova senha para o funcion
     public void setSenha(String senha) {
         this.senha = senha;
     }
