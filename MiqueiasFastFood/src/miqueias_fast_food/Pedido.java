913ccd861d265735f63f4d6f599f8d6f4a66e3a1
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-14 13:48:48 -0300

    documentação da classe Pedido

--- MiqueiasFastFood/src/miqueias_fast_food/Pedido.java
+++ MiqueiasFastFood/src/miqueias_fast_food/Pedido.java
@@ -3,17 +3,29 @@ package miqueias_fast_food;
 import java.util.ArrayList;
 
 /**
- *
  * @author guilherme
+ * Classe que representa o pedido do cliente no sistema
  */
 public class Pedido {
+    // identificador do pedido
     private final int idPedido;
+    
+    // indica se o cliente vai ou não comer no restaurante
     private boolean takeOut;
+    
+    // indica se o pedido tem desconto para as sobremesas caso seja aniversário do cliente no dia do pedido
     private boolean desconto;
+    
+    // preço total do pedido
     private double total;
+    
+    // lista de itens pedidos que compõem o pedido
     private final ArrayList<ItemPedido> itensPedidos;
+    
+    // cliente vinculado com o pedido
     private final Cliente cliente;
     
+    // Construtor da classe Pedido
     public Pedido(Cliente cliente, int idPedido, boolean takeOut) {
         this.itensPedidos = new ArrayList<>(99);
         this.cliente = cliente;
@@ -21,30 +33,37 @@ public class Pedido {
         this.takeOut = takeOut;
     }
 
+    // retorna o identificador do pedido
     public int getIdPedido() {
         return idPedido;
     }
 
+    // retorna se o cliente vai ou não comer no restaurante
     public boolean isTakeOut() {
         return takeOut;
     }
 
+    // define se o cliente vai ou não comer no restaurante
     public void setTakeOut(boolean takeOut) {
         this.takeOut = takeOut;
     }
 
+    // retorna se o pedido tem desconto
     public boolean isDesconto() {
         return desconto;
     }
 
+    // define se o pedido tem ou não desconto
     public void setDesconto(boolean desconto) {
         this.desconto = desconto;
     }
 
+    // retorna o cliente vinculado com o pedido
     public Cliente getCliente() {
         return cliente;
     }
 
+    // retorna o preço total do pedido
     public double getTotal() {
         total = 0;
         for(ItemPedido ip : itensPedidos) {
@@ -53,34 +72,43 @@ public class Pedido {
         return total;
     }
     
+    // adicionar um item no pedido
     public boolean addItem(Item item, int quantidade) {
         for(int i = 0; i < itensPedidos.size(); i++) {
             ItemPedido ip_atual = itensPedidos.get(i);
+            // se já houver o item no pedido:
             if(ip_atual.getItem().equals(item)) {
                 ip_atual.setQuantidade(quantidade + ip_atual.getQuantidade());
                 return true;
             }
         }
+        // caso contr
         itensPedidos.add(new ItemPedido(quantidade, item));
         return true;
     }
     
+    // remover um item do pedido
     public boolean delItem(Item item, int quantidade) {
         for(int i = 0; i < itensPedidos.size(); i++) {
             ItemPedido ip_atual = itensPedidos.get(i);
             if(ip_atual.getItem().equals(item)) {
                 if(quantidade >= ip_atual.getQuantidade()) {
+                    // remove o item do pedido
                     itensPedidos.remove(ip_atual);
-                    return false;
                 } else {
+                    // atualiza a quantidade do item no pedido
                     ip_atual.setQuantidade(ip_atual.getQuantidade() - quantidade);
                 }
                 return true;
             }
         }
+        
+        // adicionar tratamento de erro aqui
+        
         return false;
     }
     
+    // retorna a lista de itens pedidos no pedido
     public ArrayList<ItemPedido> getItensPedidos() {
         return itensPedidos;
     }
