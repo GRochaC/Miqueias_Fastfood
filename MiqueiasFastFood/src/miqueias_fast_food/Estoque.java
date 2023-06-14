9f4be05f7361007032ef1f38979e117e1e45962c
Caleb <67715898+CalebMartim@users.noreply.github.com>
2023-06-14 10:08:58 -0400

    Documentação de Classes Estoque e Historico
    Adicionado comentários explicando os métodos e atributos das classes assim como algumas correções de lógica

--- MiqueiasFastFood/src/miqueias_fast_food/Estoque.java
+++ MiqueiasFastFood/src/miqueias_fast_food/Estoque.java
@@ -4,50 +4,79 @@ import java.util.*;
 import java.io.*;
 
 public class Estoque{
+    
+    // HashMap para manter os ingredientes e sua respectiva quantidade 
     private static HashMap<String, Integer> ingredientes;
-    private static HashMap<String, Integer> indiceDosIngredientes;
+    
     private final static String DIRETORIO_ESTOQUE = "estoque.txt";
+    
+    // Contador de linhas para ajudar a saber em qual linha está cada ingrediente 
     private static int numeroDeLinhas = 1;
+
+    // HashMap para guardar a linha em que cada ingrediente se encontra no arquivo, para a 
+    // deleção e edição rápida
+    private static HashMap<String, Integer> indiceDosIngredientes;
 
 
     // Popula o HashMap com o conteúdo encontrado no arquivo local estoque.txt
     public static void atualizarEstoque(){
         try{
+            // Leitor de arquivos 
             BufferedReader leitor = new BufferedReader(new FileReader(DIRETORIO_ESTOQUE));
+            
+            // Cada linha vista no arquivo é salva em uma String 
             String linha;
             while((linha = leitor.readLine()) != null){
-                // Em cada linha do estoque se tem o nome de um ingrediente junto com sua quantidade
+                
+                // Em cada linha do estoque se tem o nome de um ingrediente junto com sua quantidade 
+                // separados por espaço
                 String[] conteudoDaLinha = linha.split(" ");
                 ingredientes.put(conteudoDaLinha[0], Integer.parseInt(conteudoDaLinha[1]));
 
-                // Salvamos a linha em que algo foi editado para a edição e remoção mais fácil
+                // Coloca em indiceDosIngredientes o nome do ingrediente visto e sua respectiva linha 
+                // no arquivo
                 indiceDosIngredientes.put(conteudoDaLinha[0], numeroDeLinhas);
                 ++numeroDeLinhas;
             }
+            
+            // Fecha o leitor depois de ler todas as linhas
             leitor.close();
         }catch(IOException e){
+            
+            // Caso haja algum erro na leitura do arquivo, se mostra o erro
             e.printStackTrace();
         }
     }
 
 
-    // Método para se adicionar um item no estoque
+    // Método para se adicionar um item ao estoque
     public static void adicionarItem(String nomeDoItem, int quantidadeDoItem){
 
-        // Não se pode utilizar esse método para editar itens
+        // Este método não edita itens, logo se um item já está no estoque,
+        // é avisado que ele já está
         if(ingredientes.containsKey(nomeDoItem))
             System.out.println("Item já está no estoque!");
         else{
             // Insere item no HashMap
             ingredientes.put(nomeDoItem, quantidadeDoItem);
 
-            // Insere item no arquivo
             try {
+                // Insere item no arquivo
                 BufferedWriter escritor = new BufferedWriter(new FileWriter(DIRETORIO_ESTOQUE));
-                escritor.write(nomeDoItem + quantidadeDoItem);
+                escritor.write(nomeDoItem + ' ' + quantidadeDoItem);
                 escritor.newLine();
+                
+                // Insere item no HashMap de ingredientes e guarda em qual linha ele se encontra no arquivo 
+                ingredientes.put(nomeDoItem, quantidadeDoItem);
+                indiceDosIngredientes.put(nomeDoItem, numeroDeLinhas);
+                ++numeroDeLinhas;
+
+                // Fecha o escritor
                 escritor.close();
+
             } catch (IOException e) {
+
+                // Caso haja algum erro na escrita do arquivo, se mostra o erro
                 e.printStackTrace();
             }
         }
@@ -59,65 +88,95 @@ public class Estoque{
             // Leia o arquivo e guarde cada uma das linhas num array
             File arquivo = new File(DIRETORIO_ESTOQUE);
             BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
-            String[] linhas = new String[(int) arquivo.length()];
+
+            // Usaremos um array de strings onde cada item será uma das linhas do arquivo
+            String[] linhas = new String[numeroDeLinhas-1];
             String linha;
             int indiceDeLinha = 0;
             while ((linha = leitor.readLine()) != null) {
                 linhas[indiceDeLinha++] = linha;
             }
+            
+            // Fecha o leitor quando se terminar de ler arquivo
             leitor.close();
 
-            // Modificando 
-            linhas[indiceDosIngredientes.get(nomeDoItem) - 1] = nomeDoItem + " " + novaQuantidade;
+            // Modifica no array de linhas a linha que desejamos editar, usando como auxílio 
+            // o HashMap que usamos para guardar a linha no arquivo de cada item 
+            linhas[indiceDosIngredientes.get(nomeDoItem) - 1] = nomeDoItem + ' ' + novaQuantidade;
 
             // Reescreve o arquivo
             BufferedWriter escritor = new BufferedWriter(new FileWriter(arquivo));
             for (String linhaAtualizada : linhas) {
-                escritor.write(linhaAtualizada);
-                escritor.newLine();
+                if(linhaAtualizada != ""){
+                    escritor.write(linhaAtualizada);
+                    escritor.newLine();
+                }
             }
+
+            // Fecha o escritor 
             escritor.close();
         } catch (IOException e) {
+
+            // Caso haja algum erro na escrita do arquivo, se mostra o erro
             e.printStackTrace();
         }
     }
 
     public void excluirItem(String nomeDoItem){
         try {
-            // Leia o arquivo e guarde cada uma das linhas num array exceto o
-            // índice da linha que queremos deletar
+
+            // Leia o arquivo e guarde cada uma das linhas num array exceto a linha 
+            // que possui o índice da linha que queremos deletar
+
+            // Leitor do arquivo
             File arquivo = new File(DIRETORIO_ESTOQUE);
             BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
-            String[] linhas = new String[(int) arquivo.length()];
+
+            // Array de linhas
+            String[] linhas = new String[numeroDeLinhas-1];
+
+            // Variáveis auxiliares para cada linha e o índice de cada linha
             String linha;
             int indiceDeLinha = 0;
 
             while ((linha = leitor.readLine()) != null) {
-                if(indiceDeLinha != indiceDosIngredientes.get(nomeDoItem))
+                // Se a linha atual não é a linha que queremos deletar, adiciona ela 
+                // ao array de linhas
+                if(indiceDeLinha != indiceDosIngredientes.get(nomeDoItem) - 1)
                     linhas[indiceDeLinha++] = linha;
                 else 
-                    indiceDeLinha++;
+                    ++indiceDeLinha;
             }
 
+            // Fecha o leitor
             leitor.close();
 
-            // Reescreve o arquivo
-            BufferedWriter escritor = new BufferedWriter(new FileWriter(arquivo));
+            // Reescreve o arquivo no modo de sobreescrita
+            BufferedWriter escritor = new BufferedWriter(new FileWriter(arquivo, false));
             for (String linhaAtualizada : linhas) {
-                escritor.write(linhaAtualizada);
-                escritor.newLine();
+                if(linha != ""){
+                    escritor.write(linhaAtualizada);
+                    escritor.newLine();
+                }
             }
+
+            // Fecha o escritor
             escritor.close();
         } catch (IOException e) {
+
+            // Se houver algum erro na leitura ou escrita, mostre o erro
             e.printStackTrace();
         }
     }
 
     // Mostra todos os ingredientes no estoque e sua exata quantidade
-    public static void contagemItens(){
+    public static String[] contagemItens(){
+        String relatorio[] = new String[ingredientes.size()];
+        int contador = 0;
         for(Map.Entry<String, Integer> ingrediente : ingredientes.entrySet()){
-            System.out.printf("Há " + ingrediente.getValue() + "unidade(s) de " + ingrediente + "no estoque." );
+            relatorio[contador++] = "Há " + ingrediente.getValue() + "unidade(s) de " + ingrediente + "no estoque.";
         }
+        return relatorio;
     }
 
 }
--- MiqueiasFastFood/src/miqueias_fast_food/HistoricoDePedidos.java
+++ MiqueiasFastFood/src/miqueias_fast_food/HistoricoDePedidos.java
@@ -5,45 +5,95 @@ import java.time.LocalDate;
 import java.io.*;
 
 public class HistoricoDePedidos{
-    static private LocalDate data;
-    static private ArrayList<Pedido> pedidos;
-    static final String DIRETORIO_HISTORICO = "historico.txt";
-    static final String DIRETORIO_RELATORIO = LocalDate.now() + ".txt";
 
+    // Usamos o LocalDate para adquirirmos a data do dia atual
+    static private LocalDate data;
+
+    // ArrayList para armazenarmos os pedidos feitos no dia atual
+    static private ArrayList<Pedido> pedidos;
+
+    // Arquivo que mantém o registro de todos os pedidos feitos
+    static final String DIRETORIO_HISTORICO = "historico.txt";
+
+    // O relatório será um arquivo .txt e terá como nome a data do dia atual
+    // E cada um será guardado numa pasta chamada relatorios
+    static final String DIRETORIO_RELATORIO = "relatorios/" + LocalDate.now() + ".txt";
+
+
+    // Método para adicionarmos ao nosso armazenador de pedidos um novo pedido feito
     public static void adicionarPedido(Pedido pedido){
         pedidos.add(pedido);
     }
 
-    // Adiciona ao arquivo linhas para cada um dos pedidos
+    // Ao final do dia, chamamos este método, que adiciona ao arquivo histórico cada 
+    // pedido que temos armazenado no nosso ArrayList
     public static void gerarArquivo(){
         try {
             data = LocalDate.now();
             BufferedWriter escritor = new BufferedWriter(new FileWriter(DIRETORIO_HISTORICO));
+
+           
+            /*
+             * O histórico está no formato:
+             * dd/mm/yyyy - R$xx.xx 
+             * dd/mm/yyyy - R$yy.yy
+             * ....
+             *
+             * Onde cada linha representa um pedido feito
+             */
+
             for(Pedido pedido : pedidos){
-                escritor.write(data + ": R$" + pedido.getTotal());
+                escritor.write(data + " - R$" + pedido.getTotal());
                 escritor.newLine();
             }
+
+            // Uma quebra de linha é feita no final por organização e o escritor 
+            // é fechado
+            escritor.newLine();
             escritor.close();
         } catch (IOException e) {
+            
+            // Se houver algum erro na leitura, mostrar o erro.
             e.printStackTrace();
         }
     }
 
-    // Cria um arquivo relatório com o nome sendo data atual
+    // Método para criar um arquivo detalhando exatamente os pedidos feitos no dia
     public static void gerarRelatorio(){
         try {
+
             FileWriter escritor = new FileWriter(DIRETORIO_RELATORIO);
+
+            // Variável auxiliar para termos o identificador do pedido
             int indiceDoPedido = 0;
+
             for(Pedido pedido : pedidos){
+                
+                // ArrayList de cada item pedido no pedido
                 ArrayList<ItemPedido> itensPedidos = pedido.getItensPedidos();
-                escritor.write(indiceDoPedido + ": " + pedido.getTotal() + '\n');
+
+                /* 
+                 * O formato será:
+                 * Pedido i (R$xx.xx)
+                 * - qtd(x) x(s) 
+                 * - qtd(y) y(s)
+                 */
+                
+                escritor.write("Pedido " + indiceDoPedido + ": R$" + pedido.getTotal() + '\n');
                 for(ItemPedido item : itensPedidos){
-                    escritor.write("- " + item + '\n');
+                    escritor.write("- " + item.getQuantidade() + ' ' + item.getItem() + "(s)\n");
                 }
                 ++indiceDoPedido;
+
+                // Quebra de linha no fim de cada pedido por organização
+                escritor.write('\n');
             }
+
+            // Fecha o escritor
             escritor.close();
         } catch (IOException e) {
+
+            // Se houver algum erro na leitura, mostrar o erro.
             e.printStackTrace();
         }
     }
