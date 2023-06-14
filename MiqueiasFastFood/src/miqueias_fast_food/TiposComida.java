da3e2afafd82fee508bf75c72b8d172f21244005
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-14 14:24:14 -0300

    documentação do Enumerator TiposComida

--- MiqueiasFastFood/src/miqueias_fast_food/TiposComida.java
+++ MiqueiasFastFood/src/miqueias_fast_food/TiposComida.java
@@ -1,8 +1,8 @@
 package miqueias_fast_food;
 
 /**
- *
  * @author Guilherme
+ * Enumerator que define os tipos de comida poss
  */
 public enum TiposComida {
     SANDUICHE, BEBIDA, SOBREMESA, CACHORRO_QUENTE;
