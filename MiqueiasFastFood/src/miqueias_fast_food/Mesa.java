16e1484d832052d6ae628aaf0201baf7e306bae6
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-14 12:21:46 -0300

    documentação da classe Mesa

--- MiqueiasFastFood/src/miqueias_fast_food/Mesa.java
+++ MiqueiasFastFood/src/miqueias_fast_food/Mesa.java
@@ -1,29 +1,39 @@
 package miqueias_fast_food;
 
-
+// Classe que representa uma mesa no restaurante
 public class Mesa{
+    // identificador da mesa
     private int identificador;
+    
+    // quantas pessoas cabem na mesa
     private byte tamanho;
+    
+    // indica se a mesa está ocupada ou não
     private boolean disponivel;
 
+    // Construtor da classe Mesa
     public Mesa(int _identificador, byte _tamanho, boolean _disponivel){
         this.identificador = _identificador;
         this.tamanho = _tamanho;
         this.disponivel = _disponivel;
     }
 
+    // retorna o identificador da mesa
     public int getIdentificador(){
         return this.identificador;
     }
 
+    // retorna quantas pessoas cabem na mesa
     public int getTamanho(){
         return this.tamanho;
     }
 
+    // retorna se a mesa está ocupada ou não
     public boolean getDisponivel(){
         return this.disponivel;
     }
 
+    // define se a mesa está ocupada ou não
     public void toggleDisponivel(){
         this.disponivel = !this.disponivel;
     }
