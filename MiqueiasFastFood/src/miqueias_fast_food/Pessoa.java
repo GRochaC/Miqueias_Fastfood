74e89c2df8e81357ec8f02bac1d176d9256e788a
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-14 14:22:31 -0300

    documentação da classe Pessoa

--- MiqueiasFastFood/src/miqueias_fast_food/Pessoa.java
+++ MiqueiasFastFood/src/miqueias_fast_food/Pessoa.java
@@ -1,30 +1,38 @@
 package miqueias_fast_food;
 
 /**
- *
  * @author archago
+ * Classe abstrata que representa uma pessoa no sistema
  */
 public abstract class Pessoa {
+    // cpf da pessoa
     protected String cpf;
+    
+    // nome da pessoa
     protected String nome;
 
+    // Construtor da classe Pessoa
     public Pessoa(String cpf, String nome) {
         this.cpf = cpf;
         this.nome = nome;
     }
 
+    // retorna o cpf da pessoa
     public String getCpf() {
         return cpf;
     }
 
+    // define um novo cpf para a pessoa
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
+    // retorna o nome da pessoa
     public String getNome() {
         return nome;
     }
 
+    // define um novo nome para a pessoa
     public void setNome(String nome) {
         this.nome = nome;
     }
