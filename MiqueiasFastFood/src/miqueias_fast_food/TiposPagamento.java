970932c31ac36e048f0e68be38c0b54561934572
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-14 14:26:05 -0300

    documentação do Enumerator TiposPagamento

--- MiqueiasFastFood/src/miqueias_fast_food/TiposPagamento.java
+++ MiqueiasFastFood/src/miqueias_fast_food/TiposPagamento.java
@@ -1,8 +1,8 @@
 package miqueias_fast_food;
 
 /**
- *
  * @author Guilherme
+ * Enumerator que define os tipos de pagamento poss
  */
 public enum TiposPagamento {
     DINHEIRO, CARTAO_DE_CREDITO, PIX;
