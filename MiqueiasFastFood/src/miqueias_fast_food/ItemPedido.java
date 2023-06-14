be2d1c32e47d43e8f5f808520e69fa8b50c9879c
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-14 12:01:55 -0300

    documentação da classe ItemPedido

--- MiqueiasFastFood/src/miqueias_fast_food/ItemPedido.java
+++ MiqueiasFastFood/src/miqueias_fast_food/ItemPedido.java
@@ -1,33 +1,47 @@
 package miqueias_fast_food;
 
 /**
- *
  * @author guilherme
+ * Classe auxiliar que indica a quantidade de um determinado item em um pedido
  */
 public class ItemPedido {
+    // quantidade do item no pedido
     private int quantidade;
+    
+    // subtotal do item no pedido
     private double subtotal;
+    
+    // item vinculado à classe
     private final Item item;
     
+    // Construtor da classe ItemPedido
     public ItemPedido(int quantidade, Item item) {
         this.quantidade = quantidade;
         this.item = item;
+        
+        // define o subtotal do item no pedido
         this.subtotal = item.getPreco()*quantidade;
     }
 
+    // retorna a quatidade do item no pedido
     public int getQuantidade() {
         return quantidade;
     }
 
+    // defina uma nova quantidade para o item no pedido
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
+        
+        // atualiza o subtotal do item no pedido
         this.subtotal = quantidade*item.getPreco();
     }
 
+    // retorna o subtotal do item no pedido
     public double getSubtotal() {
         return subtotal;
     }
 
+    // retorna o item vinculado à classe
     public Item getItem() {
         return item;
     }
