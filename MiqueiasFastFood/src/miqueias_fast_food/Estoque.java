a52d7c0c637e6ba1f418be02394e14c1f8367238
Caleb <67715898+CalebMartim@users.noreply.github.com>
2023-06-24 11:50:25 -0400

    Expandido atributos e consertado interação

--- MiqueiasFastFood/src/miqueias_fast_food/Estoque.java
+++ MiqueiasFastFood/src/miqueias_fast_food/Estoque.java
@@ -3,10 +3,35 @@ package miqueias_fast_food;
 import java.util.*;
 import java.io.*;
 
+class Propriedades{
+    private int quantidade;
+    private float preco;
+    private String valorNutricional, tipo; 
+    
+    public Propriedades(int quantidade, float preco, String valorNutricional, String tipo){
+        this.quantidade = quantidade;
+        this.preco = preco;
+        this.valorNutricional = valorNutricional;
+        this.tipo = tipo;
+    }
+    public int getQuantidade(){
+        return this.quantidade;
+    }
+    public float getPreco(){
+        return this.preco;
+    }
+    public String getValorNutricional(){
+        return this.valorNutricional;
+    }
+    public String getTipo(){
+        return this.tipo;
+    }
+}
+
 public class Estoque{
     
-    // HashMap para manter os ingredientes e sua respectiva quantidade 
-    private static HashMap<String, Integer> ingredientes;
+    // HashMap para manter os ingredientes e suas respectivas propriedades 
+    private static HashMap<String, Propriedades> ingredientes = new HashMap<>();
     
     private final static String DIRETORIO_ESTOQUE = "estoque.txt";
     
@@ -15,7 +40,7 @@ public class Estoque{
 
     // HashMap para guardar a linha em que cada ingrediente se encontra no arquivo, para a 
     // deleção e edição rápida
-    private static HashMap<String, Integer> indiceDosIngredientes;
+    private static HashMap<String, Integer> indiceDosIngredientes = new HashMap<>();
 
 
     // Popula o HashMap com o conteúdo encontrado no arquivo local estoque.txt
@@ -31,7 +56,8 @@ public class Estoque{
                 // Em cada linha do estoque se tem o nome de um ingrediente junto com sua quantidade 
                 // separados por espaço
                 String[] conteudoDaLinha = linha.split(" ");
-                ingredientes.put(conteudoDaLinha[0], Integer.parseInt(conteudoDaLinha[1]));
+                Propriedades propriedades = new Propriedades(Integer.parseInt(conteudoDaLinha[1]), Float.parseFloat(conteudoDaLinha[2]), conteudoDaLinha[3], conteudoDaLinha[4]);
+                ingredientes.put(conteudoDaLinha[0], propriedades);
 
                 // Coloca em indiceDosIngredientes o nome do ingrediente visto e sua respectiva linha 
                 // no arquivo
@@ -42,7 +68,6 @@ public class Estoque{
             // Fecha o leitor depois de ler todas as linhas
             leitor.close();
         }catch(IOException e){
-            
             // Caso haja algum erro na leitura do arquivo, se mostra o erro
             e.printStackTrace();
         }
@@ -50,24 +75,22 @@ public class Estoque{
 
 
     // Método para se adicionar um item ao estoque
-    public static void adicionarItem(String nomeDoItem, int quantidadeDoItem){
+    public static void adicionarItem(String nomeDoItem, int quantidadeDoItem, float preooDoItem, String valorNutricionalDoItem, String tipoDoItem){
 
         // Este método não edita itens, logo se um item já está no estoque,
         // é avisado que ele já está
         if(ingredientes.containsKey(nomeDoItem))
             System.out.println("Item já está no estoque!");
         else{
-            // Insere item no HashMap
-            ingredientes.put(nomeDoItem, quantidadeDoItem);
 
             try {
                 // Insere item no arquivo
-                BufferedWriter escritor = new BufferedWriter(new FileWriter(DIRETORIO_ESTOQUE));
-                escritor.write(nomeDoItem + ' ' + quantidadeDoItem);
+                BufferedWriter escritor = new BufferedWriter(new FileWriter(DIRETORIO_ESTOQUE, true));
+                escritor.write(String.format("%s %d %.2f %s %s", nomeDoItem, quantidadeDoItem, preooDoItem, tipoDoItem, valorNutricionalDoItem));
                 escritor.newLine();
                 
                 // Insere item no HashMap de ingredientes e guarda em qual linha ele se encontra no arquivo 
-                ingredientes.put(nomeDoItem, quantidadeDoItem);
+                ingredientes.put(nomeDoItem, new Propriedades(quantidadeDoItem, preooDoItem, valorNutricionalDoItem, tipoDoItem));
                 indiceDosIngredientes.put(nomeDoItem, numeroDeLinhas);
                 ++numeroDeLinhas;
 
@@ -83,16 +106,20 @@ public class Estoque{
     }
 
     // Método para editar a quantidade disponível de um item
-    public static void editarItem(String nomeDoItem, int novaQuantidade){
+    public static void editarItem(String nomeDoItem, int novaQuantidade, float novoPreco, String novoValorNutricional, String novoTipo){
         try {
-            // Leia o arquivo e guarde cada uma das linhas num array
+            // Leia o arquivo 
             File arquivo = new File(DIRETORIO_ESTOQUE);
             BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
 
             // Usaremos um array de strings onde cada item será uma das linhas do arquivo
             String[] linhas = new String[numeroDeLinhas-1];
+            
+            // Variáveis auxiliares para a população do array
             String linha;
             int indiceDeLinha = 0;
+            
+            // População do array
             while ((linha = leitor.readLine()) != null) {
                 linhas[indiceDeLinha++] = linha;
             }
@@ -102,10 +129,10 @@ public class Estoque{
 
             // Modifica no array de linhas a linha que desejamos editar, usando como auxílio 
             // o HashMap que usamos para guardar a linha no arquivo de cada item 
-            linhas[indiceDosIngredientes.get(nomeDoItem) - 1] = nomeDoItem + ' ' + novaQuantidade;
+            linhas[indiceDosIngredientes.get(nomeDoItem) - 1] = String.format("%s %d %.2f %s %s", nomeDoItem, novaQuantidade, novoValorNutricional, novoTipo);
 
             // Reescreve o arquivo
-            BufferedWriter escritor = new BufferedWriter(new FileWriter(arquivo));
+            BufferedWriter escritor = new BufferedWriter(new FileWriter(arquivo, false));
             for (String linhaAtualizada : linhas) {
                 if(linhaAtualizada != ""){
                     escritor.write(linhaAtualizada);
@@ -122,7 +149,7 @@ public class Estoque{
         }
     }
 
-    public void excluirItem(String nomeDoItem){
+    public static void excluirItem(String nomeDoItem){
         try {
 
             // Leia o arquivo e guarde cada uma das linhas num array exceto a linha 
@@ -140,8 +167,7 @@ public class Estoque{
             int indiceDeLinha = 0;
 
             while ((linha = leitor.readLine()) != null) {
-                // Se a linha atual não é a linha que queremos deletar, adiciona ela 
-                // ao array de linhas
+                // Se a linha atual não é a linha que queremos deletar, adiciona ela ao array de linhas
                 if(indiceDeLinha != indiceDosIngredientes.get(nomeDoItem) - 1)
                     linhas[indiceDeLinha++] = linha;
                 else 
@@ -159,9 +185,13 @@ public class Estoque{
                     escritor.newLine();
                 }
             }
+            
+            // Subtrai o número de linhas 
+            --numeroDeLinhas;
 
             // Fecha o escritor
             escritor.close();
+
         } catch (IOException e) {
 
             // Se houver algum erro na leitura ou escrita, mostre o erro
@@ -173,10 +203,18 @@ public class Estoque{
     public static String[] contagemItens(){
         String relatorio[] = new String[ingredientes.size()];
         int contador = 0;
-        for(Map.Entry<String, Integer> ingrediente : ingredientes.entrySet()){
-            relatorio[contador++] = "Há " + ingrediente.getValue() + "unidade(s) de " + ingrediente + "no estoque.";
+        for(Map.Entry<String, Propriedades> ingrediente : ingredientes.entrySet()){
+            relatorio[contador++] = "Há " + ingrediente.getValue()+ "unidade(s) de " + ingrediente + "no estoque.";
         }
         return relatorio;
+    }
+    
+    public static int getSize(){
+        return Estoque.ingredientes.size();
+    }
+    
+    public static HashMap<String, Propriedades> getEstoque(){
+        return Estoque.ingredientes;
     }
 
 }
