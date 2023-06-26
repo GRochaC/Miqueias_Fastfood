62abb792e3dd1a9b23e432bfe9f7a470a278ca54
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-06-26 11:57:40 -0300

    adição verificarCPF
    método que verifica se um CPF é válido ou não

--- MiqueiasFastFood/src/telas/Pagamento.java
+++ MiqueiasFastFood/src/telas/Pagamento.java
@@ -4,6 +4,7 @@
  */
 package telas;
 
+import java.util.InputMismatchException;
 import javax.swing.JOptionPane;
 import miqueias_fast_food.*;
 /**
@@ -62,7 +63,7 @@ public class Pagamento extends javax.swing.JFrame {
             valor_final -= desconto;
         }
         if(cbMetPagamento.getSelectedIndex() == 1) {
-            double desconto = valor_final - Math.round(valor_final);
+            double desconto = valor_final - Math.floor(valor_final);
             desconto += Math.floor(valor_final * 0.05);
             String desconto_dinheiro = String.format("-R$ %.2f (%.2f%% de desconto "
                     + "por pagamento em dinheiro)\n", desconto, desconto/pedido.getTotal()*100);
@@ -73,6 +74,61 @@ public class Pagamento extends javax.swing.JFrame {
         valor_total += String.format("\nR$ %.2f (Valor final)", valor_final);
         
         txtpValorTotalFinal.setText(valor_total);
+    }
+    
+    private boolean validarCPF(String CPF) {
+        if (CPF.equals("00000000000") ||
+            CPF.equals("11111111111") ||
+            CPF.equals("22222222222") || CPF.equals("33333333333") ||
+            CPF.equals("44444444444") || CPF.equals("55555555555") ||
+            CPF.equals("66666666666") || CPF.equals("77777777777") ||
+            CPF.equals("88888888888") || CPF.equals("99999999999") ||
+            (CPF.length() != 11))
+            return(false);
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        try {
+        // Calculo do 1o. Digito Verificador
+            sm = 0;
+            peso = 10;
+            for (i=0; i<9; i++) {
+        // converte o i-esimo caractere do CPF em um numero:
+        // por exemplo, transforma o caractere '0' no inteiro 0
+        // (48 eh a posicao de '0' na tabela ASCII)
+            num = (int)(CPF.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                dig10 = '0';
+            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+
+        // Calculo do 2o. Digito Verificador
+            sm = 0;
+            peso = 11;
+            for(i=0; i<10; i++) {
+            num = (int)(CPF.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                 dig11 = '0';
+            else dig11 = (char)(r + 48);
+
+        // Verifica se os digitos calculados conferem com os digitos informados.
+            if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
+                 return(true);
+            else return(false);
+                } catch (InputMismatchException erro) {
+                return(false);
+            }
     }
 
     /**
@@ -157,7 +213,7 @@ public class Pagamento extends javax.swing.JFrame {
         lbTelefone.setText("Telefone*:");
 
         try {
-            ftxtCPF.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("###.###.###-##")));
+            ftxtCPF.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("###########")));
         } catch (java.text.ParseException ex) {
             ex.printStackTrace();
         }
@@ -343,7 +399,7 @@ public class Pagamento extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pnlPagamento, javax.swing.GroupLayout.DEFAULT_SIZE, 800, Short.MAX_VALUE)
+            .addComponent(pnlPagamento, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -424,8 +480,18 @@ public class Pagamento extends javax.swing.JFrame {
 
     private void ftxtCPFCaretUpdate(javax.swing.event.CaretEvent evt) {//GEN-FIRST:event_ftxtCPFCaretUpdate
         // TODO add your handling code here:
-        cpf = !ftxtCPF.getText().equals("   .   .   -  ");
-        checkConfirmar();
+        if(!ftxtCPF.getText().isBlank() 
+                && ftxtCPF.getText().charAt(ftxtCPF.getText().length()-1) != ' '
+                && ftxtCPF.getText().length() == 11) {
+            cpf = validarCPF(ftxtCPF.getText());
+            if(!cpf) {
+                try{
+                ftxtCPF.setText("");
+                JOptionPane.showMessageDialog(null, "CPF inválido.", "Erro", JOptionPane.ERROR_MESSAGE);
+                } catch (IllegalStateException igException) {}
+            }
+            checkConfirmar();
+        }
     }//GEN-LAST:event_ftxtCPFCaretUpdate
 
     private void ftxtTelefoneCaretUpdate(javax.swing.event.CaretEvent evt) {//GEN-FIRST:event_ftxtTelefoneCaretUpdate
